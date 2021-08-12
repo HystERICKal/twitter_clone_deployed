a1a2057a9f1caccbc6222447878c8787197817b7
HystERICKal <nyoroerick12@gmail.com>
2021-08-12 04:46:36 +0200

    fixed issue with app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import "./App.css";
 import Widgets from "./Widgets"
-import firebase from "firebase/app"
+// import firebase from "firebase/app"
 
 function App() {
   return (
@@ -26,13 +26,13 @@ function App() {
   
 }
 
-firebase.initializeApp({
-  apiKey: "AIzaSyC1r39KbjSOlL61sKgLnHqJXf8zTRwC5w0",
-  authDomain: "twitter-clone-151ca.firebaseapp.com",
-  projectId: "twitter-clone-151ca",
-  storageBucket: "twitter-clone-151ca.appspot.com",
-  messagingSenderId: "627665935727",
-  appId: "1:627665935727:web:502acc7c009981e9f7dc9a"
-});
+// firebase.initializeApp({
+//   apiKey: "AIzaSyC1r39KbjSOlL61sKgLnHqJXf8zTRwC5w0",
+//   authDomain: "twitter-clone-151ca.firebaseapp.com",
+//   projectId: "twitter-clone-151ca",
+//   storageBucket: "twitter-clone-151ca.appspot.com",
+//   messagingSenderId: "627665935727",
+//   appId: "1:627665935727:web:502acc7c009981e9f7dc9a"
+// });
 
 export default App;
